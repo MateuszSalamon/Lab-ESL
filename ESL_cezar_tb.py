e01b65068d669718b0de4e84e514d41a0c71b01c
Mateusz <34424986+MateuszSalamon@users.noreply.github.com>
2018-12-05 17:00:59 +0100

    Working - patch - 6
    Removed while, updated conditions

--- ESL_cezar_tb.py
+++ ESL_cezar_tb.py
@@ -11,18 +11,22 @@ def testbench(vhdl_output_path=None):
 
     reset = ResetSignal(0, active=0, async=False)
     clk = Signal(bool(0))
-    key = Signal(bool(0))
+    key = Axis(1)
     state = Signal(bool(0))
     message = Axis(4)
     out = Axis(4)
     period = 10
-    state_period = 5*period
+    state_period = 100*period
+    key_period = 200*period
     #clk_gen = clk_stim(clk, period=10)
 
     low_time = int(period / 2)
     high_time = period - low_time
     state_low_time = int(state_period / 2)
     state_high_time = state_period - state_low_time
+    key_low_time = int(key_period / 2)
+    key_high_time = key_period - key_low_time
+
 
     @instance
     def drive_clk():
@@ -43,10 +47,10 @@ def testbench(vhdl_output_path=None):
     @instance
     def drive_key():
         while True:
-            yield delay(state_low_time+2)
-            key.next = 0
-            yield delay(state_high_time+1)
-            key.next = 1
+            yield delay(key_low_time)
+            key.tdata.next = 1
+            yield delay(key_high_time)
+            key.tdata.next = 0
 
 
     @instance
@@ -62,19 +66,25 @@ def testbench(vhdl_output_path=None):
 
     @instance
     def write_stim():
-        values = list(range(50, 100))
+        values = list(range(5, 10))
         i = 0
+
         yield reset.posedge
-        while i < len(values):
+        # while i < len(values):
+        #     yield clk.negedge
+        #     message.tdata.next = values[i]
+        #     if i == len(values) - 1:
+        #         message.tlast.next = 1
+        #     # else:
+        #     #     message.tlast.next = 0
+        #     # if message.tready == 1:
+        #     i += 1
+        while i < 1000:
             yield clk.negedge
-            message.tvalid.next = 1
-            message.tdata.next = values[i]
-            if i == len(values) - 1:
-                message.tlast.next = 1
-            else:
-                message.tlast.next = 0
-            if message.tready == 1:
-                i += 1
+            message.tdata.next = i
+            i += 1
+        yield clk.negedge
+        # message.tdata.next = values[i]
         yield clk.negedge
         message.tvalid.next = 0
 
@@ -101,10 +111,12 @@ def testbench(vhdl_output_path=None):
 
 
 if __name__ == '__main__':
-    trace_save_path = 'C:/Users/student/Documents/MSDC/out/testbench/'
-    vhdl_output_path = 'C:/Users/student/Documents/MSDC/out/vhdl/'
+    trace_save_path = 'C:/Users/student/Documents/MSDC/MSDC/out/testbench/'
+    vhdl_output_path = 'C:/Users/student/Documents/MSDC/MSDC/out/vhdl/'
     os.makedirs(os.path.dirname(trace_save_path), exist_ok=True)
     os.makedirs(os.path.dirname(vhdl_output_path), exist_ok=True)
+
+
 
     tb = testbench(vhdl_output_path)
     tb.config_sim(trace=True, directory=trace_save_path, name='ESL_cezar_tb')
--- ESL_cezary.py
+++ ESL_cezary.py
@@ -5,31 +5,33 @@ from myhdl import block, always_seq, Signal, intbv, enum, instances,bin
 def cezar(clk, reset, out, message, state, key):
     # state_t = enum('enc', 'dec')
     # state = Signal(state_t.enc)
-
+    max_value = 15
+    min_value = 0
 
     @always_seq(clk.posedge, reset=reset)
     def cezar_proc():
+
         out.tvalid.next = 0
         out.tlast.next = 0
         # message.tready.next = 1
-        out.tdata.next = Signal(intbv(0)[12:])
+        out.tdata.next = Signal(intbv(0)[4:])
         # if state == state_t.enc:
         if state == 1:
-            out.tdata.next = message.tdata + key
-            while((out.tdata > 15)or (out.tdata <=0)):
-                if out.tdata  > 15:
-                    out.tdata.next = out.tdata - 15
-                elif out.tdata  <= 0:
-                    out.tdata.next = out.tdata + 15
+            out.tdata.next = message.tdata + key.tdata
+            # while((out.tdata >= max_value)or (out.tdata <=min_value)):
+            if out.tdata > max_value:
+                out.tdata.next = out.tdata - max_value
+            elif out.tdata < min_value:
+                out.tdata.next = out.tdata + max_value
         # elif state == state_t.dec:
         elif state == 0:
-            # key.next= -key
-            out.tdata.next = message.tdata - key
-            while ((out.tdata > 15) or (out.tdata <= 0)):
-                if out.tdata > 15:
-                    out.tdata.next = out.tdata - 15
-                elif out.tdata <= 0:
-                    out.tdata.next = out.tdata + 15
+            # key.tdata.next= -key.tdata
+            out.tdata.next = message.tdata - key.tdata
+            # while ((out.tdata >= max_value) or (out.tdata <= min_value)):
+            if out.tdata > max_value:
+                out.tdata.next = out.tdata - max_value
+            elif out.tdata < min_value:
+                out.tdata.next = out.tdata + max_value
 
         else:
             raise ValueError("Undefined state")
